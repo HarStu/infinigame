bfbbdbc009ca9bcc5228dbed6b5b566d808e47df
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-16 18:50:18 -0400

    Navigate to chat page

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,9 +1,13 @@
+import { api } from '@/trpc/server'
+import { Chat } from '@/app/_components/chat'
 
-
-export default async function Page() {
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  console.log(`Arrived at /chat/[id]`)
+  const { id } = await props.params
+  const messages = await api.chat.loadMessages({ id })
   return (
     <div>
-      chat goes here!
+      <Chat id={id} initialMessages={messages} />
     </div>
   )
 }
--- src/app/play/[gameName]/page.tsx
+++ src/app/play/[gameName]/page.tsx
@@ -1,14 +1,13 @@
 import { api } from "@/trpc/server";
-
 import { redirect } from 'next/navigation'
 
 export default async function Page(props: { params: Promise<{ gameName: string }> }) {
   const { gameName } = await props.params
-  const chatId = api.chat.createChat({ gameName: gameName })
+  const chatId = await api.chat.createChat({ gameName: gameName })
 
   if (!chatId) {
     throw new Error(`Error creating new chat`)
   }
 
-  redirect(`/api/${chatId}`)
+  redirect(`/chat/${chatId}`)
 }
