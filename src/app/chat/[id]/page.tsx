d07445f95f1677313d7e9bcb7b58a8a753e33d1b
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-21 17:26:39 -0400

    Ensure unauthorized pages give the user a redirect to a game

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { api } from '@/trpc/server'
 import { Chat } from '@/components/chat'
+import { GameButton } from '@/components/game-button'
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   console.log(`Arrived at /chat/[id]`)
@@ -8,11 +9,9 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   // if chat isn't unowned or owned by the current user,
   // we 404 here
   const isOwner = await api.chat.checkOwnership({ id })
-  const chatInfo = await api.chat.getChat({ id })
+  const chatInfo = await api.chat.getChatWithGame({ id })
 
-  console.log(`Owner: ${isOwner}`)
-
-  if (!isOwner && chatInfo.owner !== null) {
+  if (!isOwner && chatInfo.chat.owner !== null) {
     return (
       <div className="flex flex-col justify-center items-center">
         <div className="font-bold">
@@ -21,6 +20,10 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
         <div>
           if somebody is trying to share a chat with you, ask them to use the sharable link button!
         </div>
+        <div>
+          or, play this game yourself by clicking below:
+        </div>
+        <GameButton id={chatInfo.game.id} label="play game!" />
       </div>
     )
   } else {
